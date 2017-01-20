6fc58fda59c47d37b2fb5b916f3297c7cb7b32bb
root <julian.bischof@metasfresh.com>
2017-01-20 15:30:43 +0100

    FRESH-1129 metasfresh/metasfresh#793 using curl to get the metasfresh release from our public repository and install the app

--- app/sources/start_app.sh
+++ app/sources/start_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -u
 
 DB_HOST=${DB_HOST:-db}
 APP_HOST=${APP_HOST:-app}
@@ -22,16 +23,35 @@ wait_dbms()
  done
 }
 
+run_install()
+{
+ if [[ ! -f /opt/metasfresh/metasfresh_server.jar ]]; then
+	cp -R /opt/metasfresh/dist/deploy/* /opt/metasfresh/
+        chmod 700 /opt/metasfresh/metasfresh_server.jar
+	chown root:root -R /opt/metasfresh
+	chmod -R a+w /opt/metasfresh/reports/
+ fi
+}
+
+run_db_update()
+{
+ java -jar /opt/metasfresh/dist/install/lib/de.metas.migration.cli.jar $@
+} 
+
 run_metasfresh()
 {
  cd /opt/metasfresh/ && java -jar metasfresh_server.jar
 }
 
+run_install
 
 set_properties /opt/metasfresh/metasfresh.properties
 set_properties /opt/metasfresh/local_settings.properties
 
 wait_dbms
+
+run_db_update
+
 run_metasfresh
 
 exit 0 
