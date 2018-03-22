1f661f867b42dd9a6b42c4a6876302e654989ac2
metas-jb <julian.bischof@metasfresh.com>
2018-03-22 10:37:40 +0100

    #31 add comments and infos

--- docker-src/app/sources/start_app.sh
+++ docker-src/app/sources/start_app.sh
@@ -47,6 +47,12 @@ run_db_update()
 
 run_metasfresh()
 {
+
+ #2018-03-22 jb: added '-Dcom.sun.net.ssl.enableECC=false' to java opts
+ #               gh issue: https://github.com/metasfresh/metasfresh-docker/issues/31
+ #               info: https://docs.oracle.com/cd/E26362_01/E40761/html/known-bugs-issues.html
+ #                     https://stackoverflow.com/a/19379760
+
  cd /opt/metasfresh/ && java \
  -Dsun.misc.URLClassPath.disableJarChecking=true \
  -Xmx1024M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError \
