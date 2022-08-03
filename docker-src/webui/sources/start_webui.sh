ed07a702d0db3b9859ac1b7164922b7e64aa1ebf
metas-jb <julian.bischof@metasfresh.com>
2022-08-03 09:04:38 +0200

    #78 webui apply env variable for all config.js

--- docker-src/webui/sources/start_webui.sh
+++ docker-src/webui/sources/start_webui.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# if the environment parameter for "WEBAPI_URL" is set in docker-compose.yml, replace all placeholder variables with that parameter
+# otherwise, a manual edit of './webui/sources/configs/config.js' and './webui/sources/configs/mobile/config.js' is required
+#
 if [[ ! -z $WEBAPI_URL ]]; then
-    sed -i 's,http\:\/\/MYDOCKERHOST\:PORT,'$WEBAPI_URL',g' /opt/metasfresh-webui-frontend/dist/config.js
+    find /opt/metasfresh-webui-frontend/dist/ -type f -name 'config.js' -exec sed -i 's,http\:\/\/MYDOCKERHOST\:PORT,'$WEBAPI_URL',g' {} +
 fi
 
 if [[ -f "/etc/apache2/certs/fullchain.pem" ]] && [[ -f "/etc/apache2/certs/privkey.pem" ]]; then
